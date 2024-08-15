d65c5bd8851a4ef2a6c1c3b003e9e1b2a31f0f4a
lungiswa23M <Lungiswamgeyi16@gmail.com>
2024-08-15 13:35:30 +0200

    Improved wind speed, weather description

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#current-temperature");
   let temperature = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#current-city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = temperature;
 }
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
 
 function search(event) {
   event.preventDefault();
